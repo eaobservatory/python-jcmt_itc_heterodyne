8647bc3b4cdd59e594ecc250f1728452184d3b57
Graham Bell <g.bell@eaobservatory.org>
2015-07-15 18:10:44 -1000

    Import the exception class in the __init__ file

--- lib/jcmt_itc_heterodyne/__init__.py
+++ lib/jcmt_itc_heterodyne/__init__.py
@@ -19,5 +19,5 @@ from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
 
-from .itc import HeterodyneITC
+from .itc import HeterodyneITC, HeterodyneITCError
 from .receiver import HeterodyneReceiver
