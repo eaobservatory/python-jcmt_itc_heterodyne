20f07ca1447259faead1b64b8fd506e67cacea30
Graham Bell <g.bell@eaobservatory.org>
2020-09-29 15:41:40 -1000

    model: Add plot_comparison rx, ratio etc. options

--- model/plot_comparison.py
+++ model/plot_comparison.py
@@ -19,19 +19,24 @@
 
 """
 Usage:
-    plot_comparison.py [-v|-q] --plot <plotfile> [--include-fitted] <file>...
+    plot_comparison.py [-v|-q] --plot <plotfile> [--ymax <ymax>] [--include-fitted | --ratio]  --receiver <receiver> [--sideband] <file>...
 
 Options:
     --plot <plotfile>       Filename for generated plot
+    --ymax <ymax>           Maximum value on Y-axis [default: 3000.0]
     --include-fitted        Included fitted data points
+    --ratio                 Plot as ratio rather than data + model line
     --verbose, -v           Verbose
     --quiet, -q             Quiet
+    --sideband              Sideband-specific operation
+    --receiver <receiver>   Receiver name
 """
 
 from __future__ import absolute_import, division, print_function
 
 from collections import defaultdict, namedtuple
 import logging
+import re
 import sys
 
 from docopt import docopt
@@ -43,7 +48,7 @@ from jcmt_itc_heterodyne import \
     HeterodyneITC, HeterodyneITCError, HeterodyneReceiver
 
 DataPoint = namedtuple('DataPoint', (
-    'freq', 't_rx', 't_sys', 't_rx_model', 't_sys_model', 'eta_sky'))
+    'sideband', 'freq', 't_rx', 't_sys', 't_rx_model', 't_sys_model', 'eta_sky'))
 
 logger = logging.getLogger(sys.argv[0])
 
@@ -54,13 +59,33 @@ def main():
         logging.DEBUG if args['--verbose'] else (
             logging.WARNING if args['--quiet'] else logging.INFO)))
 
+    receiver = getattr(HeterodyneReceiver, args['--receiver'])
+
+    ymax = float(args['--ymax'])
+
+    sideband_specific = args['--sideband']
+
     frequency = defaultdict(list)
 
+    pattern = re.compile('_([LU]SB)\.txt$')
+
     for filename in args['<file>']:
-        logger.debug('Reading file %s', filename)
+        if not sideband_specific:
+            sideband = None
+
+        else:
+            m = pattern.search(filename)
+
+            if not m:
+                logger.warning('Did not understand file name %s', file_)
+                continue
+
+            (sideband,) = m.groups()
+
+        logger.debug('Reading file %s (sideband %s)', filename, sideband)
         with open(filename, 'r') as f:
             for line in f:
-                point = DataPoint(*[float(x) for x in line.strip().split(' ')])
+                point = DataPoint(sideband, *[float(x) for x in line.strip().split(' ')])
                 freq_bin = int(point.freq + 0.5)
                 frequency[freq_bin].append(point)
 
@@ -72,31 +97,68 @@ def main():
             data = frequency[freq_bin]
             plt.figure()
             plt.title('{} GHz'.format(freq_bin))
-            plt.ylim(0.0, 3000.0)
+            plt.ylim(0.0, ymax)
             plt.xlim(1.0, 0.0)
+            plt.xlabel('Sky efficiency')
 
-            plt.scatter(
-                [x.eta_sky for x in data], [x.t_sys for x in data],
-                marker='.', alpha=0.25, color='r')
+            if not args['--ratio']:
+                plt.ylabel('System temperature / K')
 
-            if args['--include-fitted']:
-                plt.scatter(
-                    [x.eta_sky for x in data], [x.t_sys_model for x in data],
-                    marker='.', alpha=0.25, color='b')
+                for (sideband, color) in (zip(('LSB', 'USB'), ('g', 'y')) if sideband_specific else ((None, 'g'),)):
+                    try:
+                        for freq in (freq_bin-0.5, freq_bin, freq_bin+0.5):
+                            model_eta_sky = []
+                            model_t_sys = []
 
-            for freq in (freq_bin-0.5, freq_bin, freq_bin+0.5):
-                model_eta_sky = []
-                model_t_sys = []
+                            for tau_225 in np.arange(0.00, 0.50, 0.01):
+                                extra = {}
+                                model_t_sys.append(itc._calculate_t_sys(
+                                    receiver, freq, tau_225, 60.0,
+                                    False, None, sideband=sideband,
+                                    extra_output=extra))
+                                model_eta_sky.append(extra['eta_sky'])
 
-                for tau_225 in np.arange(0.00, 0.50, 0.01):
-                    extra = {}
-                    model_t_sys.append(itc._calculate_t_sys(
-                        HeterodyneReceiver.HARP, freq, tau_225, 60.0,
-                        False, None, None,
-                        extra_output=extra))
-                    model_eta_sky.append(extra['eta_sky'])
+                            plt.plot(model_eta_sky, model_t_sys, color=color, label='model {}'.format('' if sideband is None else sideband))
 
-                plt.plot(model_eta_sky, model_t_sys, color='g')
+                    except HeterodyneITCError:
+                        pass
+
+                if not sideband_specific:
+                    plt.scatter(
+                        [x.eta_sky for x in data], [x.t_sys for x in data],
+                        marker='.', alpha=0.25, color='r', label='data')
+                else:
+                    for (sideband, color) in zip(('LSB', 'USB'), ('r', 'b')):
+                        sb_data = [x for x in data if x.sideband == sideband]
+                        if sb_data:
+                            plt.scatter(
+                                [x.eta_sky for x in sb_data], [x.t_sys for x in sb_data],
+                                marker='.', alpha=0.25, color=color, label='data {}'.format(sideband))
+
+                if args['--include-fitted']:
+                    plt.scatter(
+                        [x.eta_sky for x in data], [x.t_sys_model for x in data],
+                        marker='.', alpha=0.25, color='k', label='fitted')
+
+            else:
+                plt.ylabel('System temperature ratio')
+
+                plt.plot([1.0, 0.0], [1.0, 1.0], color='k')
+
+                if not sideband_specific:
+                    plt.scatter(
+                        [x.eta_sky for x in data], [x.t_sys_model / x.t_sys for x in data],
+                        marker='.', alpha=0.25, color='r', label='ratio')
+
+                else:
+                    for (sideband, color) in zip(('LSB', 'USB'), ('r', 'b')):
+                        sb_data = [x for x in data if x.sideband == sideband]
+                        if sb_data:
+                            plt.scatter(
+                                [x.eta_sky for x in sb_data], [x.t_sys_model / x.t_sys for x in sb_data],
+                                marker='.', alpha=0.25, color=color, label='ratio {}'.format(sideband))
+
+            plt.legend(loc="upper left")
 
             multipage.savefig()
             plt.close()
