4d852e186e6f1ffdc3d82dfa1b30b61261812f9c
Graham Bell <g.bell@eaobservatory.org>
2020-09-29 15:42:55 -1000

    model: Update comparison scripts

--- model/compare_harp_t_sys.sh
+++ model/compare_harp_t_sys.sh
@@ -7,8 +7,8 @@ python combine_rxinfo.py -v --instrument HARP \
     --date-start 20100101 --date-end 20190920 \
     --dir rxinfo --outdir harp_combined_median --median
 
-python make_comparison.py -v --dir harp_combined_median --outdir harp_combined_median
+python make_comparison.py -v --dir harp_combined_median --outdir harp_combined_median --receiver HARP
 
-python plot_comparison.py -v --plot harp_combined_median/tsys_comparison.pdf \
+python plot_comparison.py -v --receiver HARP --plot harp_combined_median/tsys_comparison.pdf \
     harp_combined_median/comparison_median_LSB.txt \
     harp_combined_median/comparison_median_USB.txt
---
+++ model/compare_uu_t_sys.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+mkdir -p uu_combined_mean
+python model/combine_rxinfo.py -v --instrument UU \
+    --date-start 20200721 --date-end 20200918 \
+    --dir rxinfo --outdir uu_combined_mean --mean
+
+python model/make_comparison.py -v --receiver UU --sideband --if \
+    --dir uu_combined_mean --outdir uu_combined_mean
+
+python model/plot_comparison.py -v --receiver UU --sideband --ratio \
+    --plot uu_combined_mean/tsys_comparison.pdf --ymax 3.0 \
+    uu_combined_mean/comparison_mean_LSB.txt \
+    uu_combined_mean/comparison_mean_USB.txt
