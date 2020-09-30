f0391bc005bf258edaf43c7ab3e26022d676ea5c
Graham Bell <g.bell@eaobservatory.org>
2020-09-29 15:35:08 -1000

    model: Add combine_rxinfo mean and merged options

--- model/combine_rxinfo.py
+++ model/combine_rxinfo.py
@@ -18,8 +18,8 @@
 
 """
 Usage:
-    combine_rxinfo.py [-v|-q] --instrument <inst> --date <date> --dir <dir> --outdir <dir> [--median]
-    combine_rxinfo.py [-v|-q] --instrument <inst> --date-start <date> --date-end <date> --dir <dir> --outdir <dir> [--median]
+    combine_rxinfo.py [-v|-q] --instrument <inst> --date <date> --dir <dir> --outdir <dir> [--median | --mean | --merged]
+    combine_rxinfo.py [-v|-q] --instrument <inst> --date-start <date> --date-end <date> --dir <dir> --outdir <dir> [--median | --mean | --merged]
 
 Options:
     --instrument <inst>     Instrument name
@@ -31,11 +31,13 @@ Options:
     --verbose, -v           Verbose
     --quiet, -q             Quiet
     --median                Generate median rather than per-receptor data
+    --mean                  Generate mean rather than per-receptor data
+    --merged                Store per-receptor data in one file
 """
 
 from __future__ import absolute_import, division, print_function
 
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from datetime import datetime, timedelta
 import json
 import os.path
@@ -49,6 +51,12 @@ from docopt import docopt
 from omp.db.part.arc import ArcDB
 
 logger = logging.getLogger(sys.argv[0])
+
+ReceiverParam = namedtuple('ReceiverParam', ('receptors', 'filter_t_sys'))
+
+rx_params = {
+    'UU': ReceiverParam(['NU0L', 'NU1L', 'NU0U', 'NU1U'], True),
+}
 
 
 def main():
@@ -66,15 +74,21 @@ def main():
     dir_ = args['--dir']
     outdir = args['--outdir']
     use_median = args['--median']
+    use_mean = args['--mean']
+    store_merged = args['--merged']
 
     if not os.path.exists(outdir):
         raise Exception('Specified output directory does not exist')
 
+    instrument = args['--instrument']
+
     rxinfo = load_rxinfo(date_start, date_end, dir_)
 
-    obsinfo = query_db(date_start, date_end, args['--instrument'])
+    obsinfo = query_db(date_start, date_end, instrument)
 
-    output = combine_rx_db(rxinfo, obsinfo, use_median=use_median)
+    output = combine_rx_db(
+        instrument, rxinfo, obsinfo, use_median=use_median, use_mean=use_mean,
+        store_merged=store_merged)
 
     for (key, data) in output.items():
         with open(os.path.join(outdir, 'merged_{}_{}.txt'.format(*key)), 'w') as f:
@@ -82,8 +96,11 @@ def main():
                 print(*line, file=f)
 
 
-def combine_rx_db(rxinfo, obsinfo, use_median):
+def combine_rx_db(
+        instrument, rxinfo, obsinfo, use_median, use_mean, store_merged):
     output = defaultdict(list)
+
+    rx_param = rx_params.get(instrument, ReceiverParam(None, False))
 
     for obs in obsinfo:
         rxdate = rxinfo.get(str(obs['utdate']))
@@ -105,25 +122,49 @@ def combine_rx_db(rxinfo, obsinfo, use_median):
                 obs['utdate'], obs['obsnum'], obs['subsysnr'])
             continue
 
-        if use_median:
-            recep = 'median'
-            t_rx = np.median(rxss['t_rx']).item()
-            t_sys = np.median(rxss['t_sys']).item()
+        if use_median or use_mean:
+            if use_median:
+                recep = 'median'
+                t_rx = np.median(rxss['t_rx']).item()
+                t_sys = np.median(rxss['t_sys']).item()
+
+            elif use_mean:
+                recep = 'mean'
+                t_rx = mean_valid(rxss['t_rx'])
+                t_sys = mean_valid(rxss['t_sys'])
+
+            else:
+                raise Exception('Aggregate method not defined')
 
             out_key = (recep, obs['obs_sb'])
             out_data = [obs[x] for x in ['utdate', 'obsnum', 'subsysnr', 'lofreq', 'iffreq', 'rffreq']]
             out_data.extend([t_rx, t_sys])
-            out_data.extend(obs[x] for x in ['wvmtau', 'elevation'])
+            out_data.extend(obs[x] for x in ['wvmtau', 'elevation', 'sw_mode'])
+
+            output[out_key].append(out_data)
+
+        elif store_merged:
+            out_key = (instrument.lower(), 'merged')
+            receptors = rx_param.receptors
+            if receptors is None:
+                raise Exception('Receptors unknown for this instrument')
+            out_recep = {}
+            for (recep, t_rx, t_sys) in zip(rxss['recep'], rxss['t_rx'], rxss['t_sys']):
+                if 0.0 < (t_sys if rx_param.filter_t_sys else t_rx) < 1000.0:
+                    out_recep[recep] = t_sys
+            out_data = [obs[x] for x in ['utdate', 'obsnum', 'subsysnr', 'lofreq', 'iffreq', 'rffreq']]
+            out_data.extend([out_recep.get(x) for x in receptors])
+            out_data.extend(obs[x] for x in ['wvmtau', 'elevation', 'sw_mode'])
 
             output[out_key].append(out_data)
 
         else:
             for (recep, t_rx, t_sys) in zip(rxss['recep'], rxss['t_rx'], rxss['t_sys']):
-                if 0.0 < t_rx < 1000.0:
+                if 0.0 < (t_sys if rx_param.filter_t_sys else t_rx) < 1000.0:
                     out_key = (recep, obs['obs_sb'])
                     out_data = [obs[x] for x in ['utdate', 'obsnum', 'subsysnr', 'lofreq', 'iffreq', 'rffreq']]
                     out_data.extend([t_rx, t_sys])
-                    out_data.extend(obs[x] for x in ['wvmtau', 'elevation'])
+                    out_data.extend(obs[x] for x in ['wvmtau', 'elevation', 'sw_mode'])
 
                     output[out_key].append(out_data)
 
@@ -171,6 +212,7 @@ def query_db(date_start, date_end, instrument):
             ', (freq_sig_lower + freq_sig_upper) / 2 AS rffreq'
             ', (wvmtaust + wvmtauen) / 2 AS wvmtau'
             ', (elstart + elend) / 2 AS elevation'
+            ', sw_mode'
             ' FROM jcmt.COMMON JOIN jcmt.ACSIS ON COMMON.obsid = ACSIS.obsid'
             ' WHERE (utdate BETWEEN %s AND %s)'
             ' AND INSTRUME = %s'
@@ -192,6 +234,14 @@ def query_db(date_start, date_end, instrument):
 
     return ans
 
+
+def mean_valid(values):
+    valid = [x for x in values if -1000000.0 < x < 1000000.0]
+
+    if not valid:
+        return None
+
+    return np.mean(valid).item()
 
 if __name__ == '__main__':
     main()
