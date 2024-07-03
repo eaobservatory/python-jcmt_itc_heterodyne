c49b87894aa3d629659081db419933fb54e74dbe
Graham Bell <g.bell@eaobservatory.org>
2024-07-03 09:37:08 -1000

    model: Update Namakanui t_rx data filtering script
    This adds support for multiple input files, selecting a polarization
    and data where only one sideband is present.

--- model/filter_namakanui_t_rx_data.py
+++ model/filter_namakanui_t_rx_data.py
@@ -21,7 +21,7 @@
 filter_namakanui_t_rx_data - Filter receiver temperature data
 
 Usage:
-    filter_namakanui_t_rx_data [options] <filename>
+    filter_namakanui_t_rx_data [options] <filename>...
 
 Options:
     --smooth                    Apply smoothing
@@ -32,6 +32,7 @@ Options:
     --compress                  Apply compression
     --tolerance <tolerance>     Compression tolerance [default: 0.5]
     --lo2 <numbers>             LO2s to include (1-indexed, comma-separated)
+    --pol <number>              Polarization to use
     --if <frequency>            Select values matching IF frequency
     --ifrelation                Output Trx vs IF instead of vs LO
     --prefix <prefix>           Output filename prefix
@@ -69,6 +70,10 @@ def main():
         lo2s = [int(x) for x in args['--lo2'].split(',')]
         dcm_lo2 = get_dcm_lo2_mapping()
 
+    pol = None
+    if args['--pol']:
+        pol = int(args['--pol'])
+
     if_ = None
     if args['--if']:
         if_ = float(args['--if'])
@@ -77,87 +82,121 @@ def main():
     if args['--prefix']:
         prefix = '{}_'.format(args['--prefix'])
 
-    pattern = re.compile('^trx_[01]([UL])_dcm([0-9]+)$')
+    pattern = re.compile('^trx_([01])([UL])_dcm([0-9]+)$')
 
-    filename = args['<filename>']
-    if filename.endswith('.txt'):
-        format_csv = False
-    elif filename.endswith('.csv'):
-        format_csv = True
-    else:
-        raise Exception('Filename {!r} is not .txt or .csv'.format(filename))
+    data = []
+    for filename in args['<filename>']:
+        if filename.endswith('.txt'):
+            format_csv = False
+        elif filename.endswith('.csv'):
+            format_csv = True
+        else:
+            raise Exception('Filename {!r} is not .txt or .csv'.format(filename))
 
-    logger.info('Reading file %s', filename)
-    with open(filename) as f:
-        cols = None
-        header = None
-        data = []
+        logger.info('Reading file %s', filename)
+        with open(filename) as f:
+            cols = None
+            header = None
 
-        for line in f:
-            line = line.strip()
+            for line in f:
+                line = line.strip()
 
-            if not line:
-                continue
-
-            if format_csv:
-                if cols is None:
-                    cols = line.split(',')[1:]
+                if not line:
                     continue
 
-                vals = line.split(',')[1:]
+                if format_csv:
+                    if cols is None:
+                        cols = line.split(',')[1:]
+                        continue
 
-            else:
-                if line.startswith('#'):
-                    header = line[1:]
-                    continue
+                    vals = line.split(',')[1:]
 
-                if cols is None:
-                    cols = header.split(' ')
+                else:
+                    if line.startswith('#'):
+                        header = line[1:]
+                        continue
 
-                vals = line.split(' ')
+                    if cols is None:
+                        cols = header.split(' ')
 
-            assert(len(vals) == len(cols))
+                    vals = line.split(' ')
 
-            row = dict(zip(cols, vals))
+                assert(len(vals) == len(cols))
 
-            lo_freq = float(row['lo_ghz'])
-            if_freq = None
-            if 'if_ghz' in row:
-                if_freq = float(row['if_ghz'])
+                row = dict(zip(cols, vals))
 
-            if if_:
-                if if_freq is None:
-                    raise Exception('if_ghz column not present')
+                lo_freq = float(row['lo_ghz'])
+                if_freq = None
+                if 'if_ghz' in row:
+                    if_freq = float(row['if_ghz'])
 
-                if not abs(if_freq - if_) < 0.05:
-                    continue
+                if if_:
+                    if if_freq is None:
+                        raise Exception('if_ghz column not present')
 
+                    if not abs(if_freq - if_) < 0.05:
+                        continue
+
+                sum_lsb = 0.0
+                n_lsb = 0
+                sum_usb = 0.0
+                n_usb = 0
+
+                for (key, val) in row.items():
+                    match = pattern.match(key)
+                    if match:
+                        if pol is not None:
+                            if int(match.group(1)) != pol:
+                                continue
+
+                        if lo2s:
+                            lo2 = dcm_lo2[int(match.group(3))]
+
+                            if lo2 not in lo2s:
+                                continue
+
+                        t_rx = float(val)
+
+                        if match.group(2) == 'L':
+                            sum_lsb += t_rx
+                            n_lsb += 1
+
+                        elif match.group(2) == 'U':
+                            sum_usb += t_rx
+                            n_usb += 1
+
+                data.append(Measurement(
+                    lo_freq,
+                    if_freq,
+                    (None if not n_lsb else (sum_lsb / n_lsb)),
+                    (None if not n_usb else (sum_usb / n_usb))))
+
+    # Average data from multiple files?
+    if len(args['<filename>']) > 1:
+        filtered = []
+        keys = sorted(set((x.lo, x.if_) for x in data))
+        for (lo, if_) in keys:
             sum_lsb = 0.0
-            n_lsb = 0
             sum_usb = 0.0
+            n_lsb = 0
             n_usb = 0
 
-            for (key, val) in row.items():
-                match = pattern.match(key)
-                if match:
-                    if lo2s:
-                        lo2 = dcm_lo2[int(match.group(2))]
-
-                        if lo2 not in lo2s:
-                            continue
-
-                    t_rx = float(val)
-
-                    if match.group(1) == 'L':
-                        sum_lsb += t_rx
+            for entry in data:
+                if (entry.lo == lo and entry.if_ == if_):
+                    if entry.lsb is not None:
+                        sum_lsb += entry.lsb
                         n_lsb += 1
-
-                    elif match.group(1) == 'U':
-                        sum_usb += t_rx
+                    if entry.usb is not None:
+                        sum_usb += entry.usb
                         n_usb += 1
 
-            data.append(Measurement(
-                lo_freq, if_freq, sum_lsb / n_lsb, sum_usb / n_usb))
+            filtered.append(Measurement(
+                lo,
+                if_,
+                (None if not n_lsb else (sum_lsb / n_lsb)),
+                (None if not n_usb else (sum_usb / n_usb))))
+
+        data = filtered
 
     base_suffix = ''
 
@@ -186,8 +225,24 @@ def main():
 
     else:
         key = 'lo'
-        sidebands = ('lsb', 'usb')
         data = sorted(data, key=lambda x: x.lo)
+
+        have_lsb = False
+        have_usb = False
+        for entry in data:
+            if entry.lsb is not None:
+                have_lsb = True
+            if entry.usb is not None:
+                have_usb = True
+
+        if have_lsb and have_usb:
+            sidebands = ('lsb', 'usb')
+        elif have_lsb:
+            sidebands = ('lsb',)
+        elif have_usb:
+            sidebands = ('usb',)
+        else:
+            raise Exception('can not find sideband')
 
     for sideband in sidebands:
         suffix = base_suffix
